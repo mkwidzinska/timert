027e654718aef7b75c443dd26bc748a802a24980
Gosia Kwidzinska <malgorzata.kwidzinska@gmail.com>
2013-07-25 10:30:42 +0200

    Added more details to one of the test in the argument_parser_spec

--- spec/argument_parser_spec.rb
+++ spec/argument_parser_spec.rb
@@ -5,6 +5,7 @@ describe ArgumentParser do
   let(:ap_with_empty_array) { ArgumentParser.new([]) }
   let(:ap_with_start) { ArgumentParser.new(['start']) }
   let(:ap_with_start_at) { ArgumentParser.new(['start', '16:50']) }
+  let(:ap_with_stop_at) { ArgumentParser.new(['stop', '19']) }
 
   it 'should be initialized with one array argument' do
     expect { ap_with_empty_array }.to_not raise_error
@@ -14,10 +15,18 @@ describe ArgumentParser do
     expect(ap_with_start.action).to eq('start')
   end
 
-  context 'when second argument is passed to a time-associated method' do
+  context 'when a time argument is given with the start action' do
     it 'should have argument method that returns numeric representation of that time' do
       Timecop.freeze(Time.new(2013, 4, 6, 16, 50)) do
         expect(ap_with_start_at.argument).to eq(Time.now.to_i)
+      end
+    end
+  end
+
+  context 'when a time argument is given with the stop action' do
+    it 'should have argument method that returns numeric representation of that time' do
+      Timecop.freeze(Time.new(2013, 4, 6, 19)) do
+        expect(ap_with_stop_at.argument).to eq(Time.now.to_i)
       end
     end
   end
