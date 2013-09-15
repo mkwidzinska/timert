5c6be25fbc37bface561e8d37b3d121ca7d06e9d
Maciej Malecki <smt116@gmail.com>
2013-09-15 22:14:47 +0200

    Use more obvious names for dates in let methods in report specs.
    2013-02-20 is Wednesday so it is more 'human' to use it instead of
    'today' (which is not true).

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -3,40 +3,40 @@ require 'timecop'
 require_relative '../lib/timert/report'
 
 describe Timert::Report do
-  let(:today) { Date.new(2013, 2, 20) }
-  let(:yesteday) { Date.new(2013, 2, 19) }
+  let(:wednesday) { Date.new(2013, 2, 20) }
+  let(:tuesday) { Date.new(2013, 2, 19) }
   let(:database) { double }
 
   let(:first_day) do
     double(total_elapsed_time: 3.5 * 3600, 
-      date: today,
+      date: wednesday,
       tasks: ["emails", "meeting"],
       intervals: [
-        {"start" => time(today, 16), "stop" => time(today, 18, 30)},
-        {"start" => time(today, 19), "stop" => time(today, 20)}
+        {"start" => time(wednesday, 16), "stop" => time(wednesday, 18, 30)},
+        {"start" => time(wednesday, 19), "stop" => time(wednesday, 20)}
       ])
   end
 
   let(:second_day) do
     double(total_elapsed_time: 7 * 3600, 
-      date: yesteday,
+      date: tuesday,
       tasks: ["reports", "bugs"],
       intervals: [
-        {"start" => time(yesteday, 11, 30), "stop" => time(yesteday, 16)},
-        {"start" => time(yesteday, 16, 30), "stop" => time(yesteday, 19, 30)}
+        {"start" => time(tuesday, 11, 30), "stop" => time(tuesday, 16)},
+        {"start" => time(tuesday, 16, 30), "stop" => time(tuesday, 19, 30)}
       ])
   end
 
   before(:each) do
-    Timecop.freeze(today)
+    Timecop.freeze(wednesday)
   end
 
   after(:each) do
     Timecop.return    
   end
 
-  it 'should generate report for today' do
-    database.should_receive(:day).with(today).and_return(first_day)
+  it 'should generate report for wednesday' do
+    database.should_receive(:day).with(wednesday).and_return(first_day)
     
     report = Timert::Report.generate(database)
     expect(report.include?("2013-02-20")).to eq(true)
@@ -46,7 +46,7 @@ describe Timert::Report do
   end
 
   it 'should generate report for any past day' do    
-    database.should_receive(:day).with(yesteday).and_return(second_day)
+    database.should_receive(:day).with(tuesday).and_return(second_day)
 
     report = Timert::Report.generate(database, -1)
     expect(report.include?("2013-02-19")).to eq(true)
